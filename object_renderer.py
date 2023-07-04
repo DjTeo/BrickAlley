eabf90273b66f7135825583842c88b6082877517
Ntoulias Chris <ntouliaschris@gmail.com>
2023-07-04 20:32:50 +0300

    Added hands on screen

--- object_renderer.py
+++ object_renderer.py
@@ -12,6 +12,9 @@ class ObjectRenderer:
                                             (GAME_WIDTH, HALF_HEIGHT))
         self.sky_offset = 0
         self.blood_screen = Helper.LoadTexture('blood_texture.png', RES)
+        self.left_hand=Helper.LoadSprite('leftHand.png',(GAME_WIDTH/4,GAME_HEIGHT/2))
+        self.right_hand=Helper.LoadSprite('rightHand.png',(GAME_WIDTH/4,GAME_HEIGHT/2))
+        
         # self.digit_size = 90
         # self.digit_images = [Helper.LoadTexture(f'resources/textures/digits/{i}.png', [self.digit_size] * 2)
         #                      for i in range(11)]
@@ -23,6 +26,7 @@ class ObjectRenderer:
         self.draw_background(screen)
         self.render_game_objects(screen)
         self.draw_player_health(screen)
+        self.draw_hands(screen)
 
     def win(self):
         #     self.screen.blit(self.win_image, (0, 0))
@@ -57,6 +61,11 @@ class ObjectRenderer:
         pygame.draw.rect(screen, BROWN,
                          (0, HALF_HEIGHT, GAME_WIDTH, GAME_HEIGHT))
 
+    def draw_hands(self,screen):
+        screen.blit(self.left_hand,(325,530))
+        screen.blit(self.right_hand,(885,530))
+        
+        
     def render_game_objects(self, screen):
         list_objects = sorted(self.game.raycasting.objects_to_render,
                               key=lambda t: t[0],
