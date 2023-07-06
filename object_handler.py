c3704b20edcdc98305a02999c89837a84ae97bb1
Ntoulias Chris <ntouliaschris@gmail.com>
2023-07-06 08:04:19 +0300

    Obstacle Spawner- 1st draft Collision Checking Next up: Variety in enemies

--- object_handler.py
+++ object_handler.py
@@ -1,6 +1,7 @@
 from sprite_object import *
 # from npc import *
 from random import choices, randrange
+import random
 
 
 class ObjectHandler:
@@ -8,23 +9,25 @@ class ObjectHandler:
         self.game = game
         self.sprite_list = []
         self.npc_list = []
-        self.npc_sprite_path = 'resources/sprites/npc/'
-        self.static_sprite_path = 'resources/sprites/static_sprites/'
-        self.anim_sprite_path = 'resources/sprites/animated_sprites/'
+        self.npc_sprite_path = 'assets/sprites/npc/'
+        self.static_sprite_path = 'assets/sprites/static_sprites/'
+        self.anim_sprite_path = 'assets/sprites/animated_sprites/'
         add_sprite = self.add_sprite
+        
         # add_npc = self.add_npc
         self.npc_positions = {}
 
         # spawn npc
-        self.enemies = 20  # npc count
+        self.enemies = 0  # npc count
         # self.npc_types = [SoldierNPC, CacoDemonNPC, CyberDemonNPC]
         self.weights = [70, 20, 10]
         self.restricted_area = {(i, j) for i in range(10) for j in range(10)}
         # self.spawn_npc()
 
         # sprite map
-        # add_sprite(AnimatedSprite(game))
-        # add_sprite(AnimatedSprite(game, pos=(1.5, 1.5)))
+        #coords are normalized into squares. if x>3 or x<1 then they go outside the map
+        #add_sprite(AnimatedSprite(game))
+        #add_sprite(AnimatedSprite(game, pos=(1.5, 1.5)))
         # add_sprite(AnimatedSprite(game, pos=(1.5, 7.5)))
         # add_sprite(AnimatedSprite(game, pos=(5.5, 3.25)))
         # add_sprite(AnimatedSprite(game, pos=(5.5, 4.75)))
@@ -32,7 +35,7 @@ class ObjectHandler:
         # add_sprite(AnimatedSprite(game, pos=(7.5, 5.5)))
         # add_sprite(AnimatedSprite(game, pos=(14.5, 1.5)))
         # add_sprite(AnimatedSprite(game, pos=(14.5, 4.5)))
-        # add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(14.5, 5.5)))
+        #add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'ballSprite/1.png', pos=(14.5, 1.5)))
         # add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(14.5, 7.5)))
         # add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(12.5, 7.5)))
         # add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(9.5, 7.5)))
@@ -47,7 +50,7 @@ class ObjectHandler:
         # add_sprite(AnimatedSprite(game, pos=(1.5, 24.5)))
 
         # npc map
-        # add_npc(SoldierNPC(game, pos=(11.0, 19.0)))
+        #add_npc(SoldierNPC(game, pos=(11.0, 19.0)))
         # add_npc(SoldierNPC(game, pos=(11.5, 4.5)))
         # add_npc(SoldierNPC(game, pos=(13.5, 6.5)))
         # add_npc(SoldierNPC(game, pos=(2.0, 20.0)))
@@ -55,7 +58,6 @@ class ObjectHandler:
         # add_npc(CacoDemonNPC(game, pos=(5.5, 14.5)))
         # add_npc(CacoDemonNPC(game, pos=(5.5, 16.5)))
         # add_npc(CyberDemonNPC(game, pos=(14.5, 25.5)))
-
     def spawn_npc(self):
         for i in range(self.enemies):
                 npc = choices(self.npc_types, self.weights)[0]
@@ -69,9 +71,27 @@ class ObjectHandler:
         [sprite.update() for sprite in self.sprite_list]
         [npc.update() for npc in self.npc_list]
         # self.check_win()
-
+        print(self.enemies)
     def add_npc(self, npc):
         self.npc_list.append(npc)
 
     def add_sprite(self, sprite):
         self.sprite_list.append(sprite)
+        self.enemies+=1
+    
+    def remove_sprite(self):
+        del self.sprite_list[0]
+        self.enemies-=1
+        
+
+    def closest_enemy(self):
+        return self.sprite_list[0].x,self.sprite_list[0].y
+    
+    def spawn_obstacle(self): 
+        y=1.1+1.7*random.random()
+        x=self.game.player.x+10+y
+        self.add_sprite(AnimatedSprite((self.game), pos=(x,y)))
+        
+        
+    
+   
--- player.py
+++ player.py
@@ -1,9 +1,9 @@
 from constants import *
 import pygame
 import math
-
 from helper import Helper
 
+from object_handler import *
 
 class Player:
 
@@ -15,7 +15,9 @@ class Player:
         self.health = PLAYER_MAX_HEALTH
         self.health_recovery_delay = 700
         self.player_hit = False
+        
         self.time_prev = pygame.time.get_ticks()
+        self.respawn_timer=2000
         self.player_pain = Helper.PrepareSound("player_pain.wav")
         self.blood_screen = Helper.LoadTexture('blood_texture.png', RES)
 
@@ -42,7 +44,8 @@ class Player:
         if time_now - self.time_prev > self.health_recovery_delay:
             self.time_prev = time_now
             return True
-
+    
+                
     def check_win(self):
         if self.x >= END_DISTANCE:
             self.game.victory = True
@@ -79,36 +82,65 @@ class Player:
         if self.y + dy < PLAYER_RIGHT_END and self.y + dy > PLAYER_LEFT_END:
             self.y += dy
 
+    def check_collision(self):
+        
+        if self.game.object_handler.closest_enemy()[1]<self.y+0.1 and self.game.object_handler.closest_enemy()[1]>self.y-0.1:
+            will_be_hit=True
+            if will_be_hit and self.game.object_handler.closest_enemy()[0]<=self.x+0.3:
+                #Player.get_damage()
+                print('hit!')
+                self.game.object_handler.remove_sprite()
+                will_be_hit=False
+        elif self.game.object_handler.closest_enemy()[0]<=self.x+0.3:
+            self.game.object_handler.remove_sprite()
+            print('missed')
+        else:
+            will_be_hit=False
+        
+        
+            
+        
+
     def update(self, delta_time):
         self.movement(delta_time)
         # self.recover_health()
         self.check_win()
+        
+        if self.game.object_handler.sprite_list:
+            self.check_collision()
+            
+        if self.game.object_handler.enemies<5 and self.timer-self.time_prev>self.respawn_timer:
+            self.game.object_handler.spawn_obstacle()
+            self.time_prev=self.timer
+        
+        
+        
 
     def draw(self, screen):
-        self.timer = ((pygame.time.get_ticks() - self.time_prev) //
-                      self.animation_frequency) % self.animation_stages
+        self.timer = pygame.time.get_ticks()  
 
-        if self.timer == 0:
+        if (self.timer//self.animation_frequency) % self.animation_stages == 0:
             screen.blit(self.right_hand,
                         (GAME_WIDTH - GAME_WIDTH / 2, GAME_HEIGHT * 0.65))
             screen.blit(self.left_hand, (GAME_WIDTH / 4, GAME_HEIGHT * 0.65))
 
-        elif self.timer == 1:
+        elif (self.timer//self.animation_frequency) % self.animation_stages  == 1:
             screen.blit(self.left_hand_up,
                         (GAME_WIDTH * 0.15, GAME_HEIGHT / 2))
 
-        elif self.timer == 2:
+        elif (self.timer//self.animation_frequency) % self.animation_stages == 2:
             screen.blit(self.right_hand,
                         (GAME_WIDTH - GAME_WIDTH / 2, GAME_HEIGHT * 0.65))
             screen.blit(self.left_hand, (GAME_WIDTH / 4, GAME_HEIGHT * 0.65))
-        elif self.timer == 3:
+        elif (self.timer//self.animation_frequency) % self.animation_stages == 3:
             screen.blit(self.right_hand_up,
                         (GAME_WIDTH * 0.5, GAME_HEIGHT / 2))
 
         if self.player_hit:
             screen.blit(self.blood_screen, (0, 0))
             self.player_hit = False
-
+            
+            
     @property
     def pos(self):
         return self.x, self.y
